22b67f423ac4adef3b5870c976179deb0e82d25b
Jonathan Santos <jonathan01347@gmail.com>
2024-01-17 21:48:11 -0300

    feat(app)added middleware error

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
+
+app.use((err, _req, res, _next) => {
+  if (err.message.split('').includes('|')) {
+    const [status, message] = err.message.split('|');
+    res.status(Number(status)).json({ error: message });
+  } else {
+    console.error('Erro não Mapeado:', err);
+    console.error('Erro não Mapeado:', err.message);
+    res.status(500).json({ error: err.message });
+  }
+  
+});
+
 const server = app.listen(PORT, () => console.log(
   `Server is running on PORT: ${PORT}`,
 ));
