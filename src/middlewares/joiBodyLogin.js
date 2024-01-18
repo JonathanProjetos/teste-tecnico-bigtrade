71357895f9dac4d06e686b4f8007784d3d7d59dc
Jonathan Santos <jonathan01347@gmail.com>
2024-01-18 11:54:54 -0300

    feat(joiBodyLogin)implemented a middleware to check input data from login.

---
+++ src/middlewares/joiBodyLogin.js
@@ -0,0 +1,25 @@
+const Joi = require('joi');
+
+const bodyLoginUser = (dados) => {
+  const userSchema = Joi.object({
+    email: Joi.string().required().email().messages({
+      'string.empty': '400|The "email" field cannot be empty.',
+      'any.required': '400|The "email" field is mandatory.',
+      'string.email': '400|The "email" must be in the format test@test.com.',
+    }),
+    password: Joi.string().min(6).required().messages({
+      'string.empty': '400|The "password" field cannot be empty.',
+      'any.required': '400|The "password" field is mandatory.',
+      'string.min': '400|The "password" field must be at least 6 characters.',
+    }),
+  });
+
+  const { error, value } = userSchema.validate(dados);
+
+  if (error) {
+    throw error;
+  }
+  return value;
+};
+
+module.exports = bodyLoginUser;
