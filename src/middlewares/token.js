2300548d817bad3c89b138c40caab4a725866365
Jonathan Santos <jonathan01347@gmail.com>
2024-01-18 11:57:04 -0300

    feat(token)implement middleware from create and check token.

---
+++ src/middlewares/token.js
@@ -0,0 +1,28 @@
+const jsonwebtoken = require('jsonwebtoken');
+require('dotenv').config();
+
+const secret = process.env.JWT_SECRET;
+
+const jwt = {
+  // Gera o token de autenticação.
+  generateToken: (email) => {
+    const token = jsonwebtoken.sign({ email }, secret, {
+      expiresIn: '1d',
+      algorithm: 'HS256',
+    });
+    return token;
+  },
+
+  // Valida o token de autenticação.
+  validateToken: (token) => {
+    if (!token) throw new Error('404|Token not found');
+    try {
+      const payload = jsonwebtoken.verify(token, secret);
+      return payload;
+    } catch (error) {
+      throw new Error('401|Token must be a valid token');
+    }
+  },
+};
+
+module.exports = jwt;
