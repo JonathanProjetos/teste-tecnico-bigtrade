c0772eb710ae37bccbe0ca41caa3e3e021492d1c
Jonathan Santos <jonathan01347@gmail.com>
2024-01-19 15:53:19 -0300

    feat(UserController)Add/update user CRUD operations in UserController.js

--- src/controllers/user/UserController.js
+++ src/controllers/user/UserController.js
@@ -7,6 +7,27 @@ const UserController = {
     res.status(201).json(newUser);
   },
 
+  updateUser: async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    const { email } = req;
+    const updateUser = await UserServices.updateUser(body, id, email);
+    res.status(200).json(updateUser);
+  },
+
+  getUserById: async (req, res) => {
+    const { id } = req.params;
+    const user = await UserServices.getUserById(id);
+    res.status(200).json(user);
+  },
+
+  deleteUser: async (req, res) => {
+    const { id } = req.params;
+    const { email } = req;
+    const user = await UserServices.deleteUser(email, id);
+    res.status(200).json(user);
+  },
+
 };
 
 module.exports = UserController;
