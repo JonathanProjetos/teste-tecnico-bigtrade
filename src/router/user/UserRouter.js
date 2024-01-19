88c269b54629c898dd3eaaa4be3fea9f283671d8
Jonathan Santos <jonathan01347@gmail.com>
2024-01-19 15:55:36 -0300

    feat(UserRouter)Add routes for updating, getting, and deleting users

--- src/router/user/UserRouter.js
+++ src/router/user/UserRouter.js
@@ -1,9 +1,13 @@
 const { Router } = require('express');
 const UserController = require('../../controllers/user/UserController');
+const validateToken = require('../../middlewares/verifyToken');
 
 const userRouter = Router();
 
 userRouter.post('/users', UserController.createUser);
+userRouter.put('/users/:id', validateToken.verifyToken, UserController.updateUser);
+userRouter.get('/users/:id', UserController.getUserById);
+userRouter.delete('/users/:id', validateToken.verifyToken, UserController.deleteUser);
 
 
 module.exports = userRouter;
